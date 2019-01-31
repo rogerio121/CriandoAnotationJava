8871e558385fd4ac0a7eb3977f9d95d1fddd3277
Lucas Vieira Ferreira <lucas.vieira.ferreira@usp.br>
2019-01-30 23:58:15 -0200

    Implementacao de foreach com lambda

--- src/main/java/testesJava/UserAnotation.java
+++ src/main/java/testesJava/UserAnotation.java
@@ -1,13 +1,9 @@
 package testesJava;
-import com.sun.javaws.Main;
-import jdk.nashorn.internal.objects.NativeArray;
+
 import org.reflections.Reflections;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @CustomAnotation(nome = "nomePassado")
 public class UserAnotation {
@@ -24,12 +20,34 @@ public class UserAnotation {
         Reflections reflections = new Reflections("testesJava");
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(CustomAnotation.class);
 
+        System.out.println(getClasseByAnnotation("nomePassado", classes));
+
+    }
+
+    public static String getClasseByAnnotation(String annotationName, Set<Class<?>> classes){
+
+    	String retorno = "";
+
+    	Optional<Class<?>> optional = classes.stream().
+    			filter(x -> x.getAnnotation(CustomAnotation.class)
+    							.nome()
+    							.equalsIgnoreCase(annotationName))
+    							.findFirst();
+    	
+    	if(optional.isPresent()){
+    		retorno = optional.get().getName();
+    		System.out.println("Achou classe " + retorno);
+    	}
+
+    	/*
         for (Class<?> cl : classes) {
             CustomAnotation classeEncontrada = cl.getAnnotation(CustomAnotation.class);
-            System.out.println(classeEncontrada.toString());
-        }
-
-        System.out.printf(classes.toString());
-        //String key = challengeClass.getAnnotation(CustomAnotation.class).key();
+            if(classeEncontrada.nome().equalsIgnoreCase(annotationName)) {
+                retorno = cl.toString();
+            }
+        }*/
+        
+        return retorno;
     }
+
 }
